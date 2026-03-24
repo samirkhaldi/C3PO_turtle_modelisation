8839967ce30ee973b39323f252a129a6b0ce7554
Samir Khaldi <samir.khaldi@esciencefactory.com>
2026-03-24 09:06:27 +0100

    script fonctionnel

--- sparql_test/test.py
+++ sparql_test/test.py
@@ -4,21 +4,12 @@ from rdflib import Graph
 QS = {
     1: ("35000", "23000"),
     2: ("15",),
-    3 : ('1240', 'SHOPPING', 'Milan', 'LUMEN_Meet1', '2025-09-23', '2025-09-24',),
-    # EXPECT: ('1240', 'SHOPPING', 'Milan', 'LUMEN_Meet1', '2025-09-23', '2025-09-24')
-    # REPONSE: set()
-    # QS numero 3 est KO
+    3 : ("1240", "SHOPPING", "Milan", "LUMEN_Meet1", "2025-09-23", "2025-09-24"),
     4: ('0'),
-    5: ('31'), #Ne fonctionne pas 
-    #log : EXPECT: 31
-    # REPONSE: {'31'}
-    # QS numero 5 est KO
+    5: ('31',), 
     6: ("100", "1000", "10000",),
     7: ('28400',),
     8: ("Yann",),
-    # EXPECT: Yann
-    # REPONSE: {'Yann'}
-    # QS numero 8 est KO 
     9: ("Yann",),
     10: ("32",),
     11: ('10000',),
@@ -27,15 +18,9 @@ QS = {
     14: ("2",),
     15: ('RP3', 'RP1', 'P1', 'RP2',),
     16: ('143.3333333333333333333333333',),
-    # EXPECT: 143.3333333333333333333333333
-    # REPONSE: {'143.3333333333333333333333333'}
-    # QS numero 16 est KO
     17: ('WP4', 'WP2', 'WP3',),
     18: ('LUMEN_Meet1', 'LUMEN_Meet3'),
     19: ('1240',),
-    # EXPECT: 1240
-    # REPONSE: {'1240'}
-    # QS numero 19 est KO
     20: ("Milan",),
     21: ('10', '50', '32',),
     22: ("SHOPPING",),
@@ -49,14 +34,14 @@ QS = {
 g = Graph()
 g.parse("test.ttl", format="turtle")
 
-for i in range(3, 4):
+for i in range(1, 27):
     with open(f'query/Q{i}_sparql.txt', 'r', encoding='utf-8') as f:
         query = f.read()
 
     responses = set()
     for row in g.query(query):
         for value in row:
-            responses.add(value)
+            responses.add(str(value))
 
     print("EXPECT:", QS[i])
     print("REPONSE:", responses)
